0135cafdfaeb62965d5d7b0d3459ca476f3b67d7
Ali KARS <alikars41@gmail.com>
2024-02-15 22:06:50 +0300

    Basit bir sayfa tasarımı yapıldı. Sayfalar yapılırken TextView,chips, ImageView ve Button kullanıldı. Sayfanın renkleri için düzenlemeler colors.xml dosyası içerisinde tanımlandı ve kullanıldı. Sayfa içerisindeki textler strings.xml dosyası içerisinde tanımlandı. Çoklu dil desteği için res klasörünün altına values-tr dosyası tanımlandı. Burada oluşturulan strings.xml dosyası içerisinde aynı öncesinde İngilizce tanımlanan değişkenler Türkçe olarak oluşturuldu.Yazıtipi için GoogleFonts dan bir yazıtipi seçilerek projeye eklendi. Çoklu ekran desteği için res altına layout-xlarge klasörü oluşturuldu ve halihazırda oluşturulmuş olan activity_main.xml dosyası buraya kopyalanıp Nexus 10 tablet görünümü için küçük değişiklikler yapıldı. Gece-gündüz modları için de ayrıca renk tanımlamaları yapıldı. Uygulama iconu değiştirildi.

---
+++ app/src/main/java/com/akliars/tasarimcalismasi/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.akliars.tasarimcalismasi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //Kotlin sınıfı içinde R = res klasörü
+        //Xml dosyası içinde @ = res klasörü
+    }
+}
